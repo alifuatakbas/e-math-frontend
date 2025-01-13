6a8f3d118cac07d557c4fa61d78e9cffaf21575f
Ali Fuat Akbas <alifuatakbas-3@hotmail.com>
2025-01-13 04:19:38 +0300

    up

--- app/components/RegisterForm.tsx
+++ app/components/RegisterForm.tsx
@@ -25,7 +25,7 @@ const Register: React.FC = () => {
     };
 
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/register`, {
+      const response = await fetch(`https://backend-emath-production.up.railway.app/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
