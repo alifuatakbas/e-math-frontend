88f03d99ba2d616180367ed6ffa793937a26bd22
Ali Fuat Akbas <alifuatakbas-3@hotmail.com>
2025-01-15 01:41:12 +0300

    added reset-password

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import ResetPassword from './../components/Reset-password';
+
+export default function ResetPasswordPage() {
+    return (
+        <main>
+            <ResetPassword />
+        </main>
+    );
+}
