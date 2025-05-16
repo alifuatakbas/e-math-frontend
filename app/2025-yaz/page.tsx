2efe36001b3fdb71dfc27bbae351175c45f3602b
Ali Fuat Akbas <alifuatakbas-3@hotmail.com>
2025-05-17 00:21:28 +0300

    mobilenavbar

--- app/2025-yaz/page.tsx
+++ app/2025-yaz/page.tsx
@@ -24,7 +24,8 @@ const SummerProgram2025 = () => {
           <div className={styles.programCard}>
             <h2>Hedef Kitle</h2>
             <div className={styles.content}>
-              <p>Önümüzdeki yıl 4, 5, 6 veya 7.sınıf&apos;a geçecek matematik alanında yetenekli ve ilgili olan olan öğrencileri hızlı bir şekilde hedeflerine ulaştırıyoruz.</p>
+              <p>Önümüzdeki yıl 4, 5, 6 veya 7.sınıf&apos;a geçecek matematik alanında yetenekli ve ilgili olan olan
+                öğrencileri hızlı bir şekilde hedeflerine ulaştırıyoruz.</p>
             </div>
           </div>
 
@@ -50,7 +51,19 @@ const SummerProgram2025 = () => {
         </div>
 
         <div className={styles.ctaSection}>
-          <h2>İletişim için: +90 501 087 12 13</h2>
+          <div className={styles.contactInfo}>
+            <div className={styles.contactIcon}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                   stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path
+                    d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+              </svg>
+            </div>
+            <div className={styles.contactText}>
+              <h3>Bize Ulaşın</h3>
+              <p>+90 501 087 12 13</p>
+            </div>
+          </div>
           <button className={styles.ctaButton} onClick={handleApply}>Başvuru Yap</button>
         </div>
       </div>
